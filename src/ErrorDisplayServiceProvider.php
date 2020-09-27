eeec9a7db748cec07b86a8d4d9ae0fa835c88351
王正东 <wangzhengdong@ecmoban.com>
2020-09-27 17:51:19 +0800

    添加错误参数为可选

--- src/ErrorDisplayServiceProvider.php
+++ src/ErrorDisplayServiceProvider.php
@@ -15,11 +15,11 @@ class ErrorDisplayServiceProvider extends ServiceProvider
 	 */
 	public function boot()
 	{
-        RC_Hook::add_action('handle_404_error', function ($msg, $url) {
+        RC_Hook::add_action('handle_404_error', function ($msg, $url = null) {
             ErrorDisplay::http_error(404, $msg);
         });
 
-        RC_Hook::add_action('handle_500_error', function ($msg, $url) {
+        RC_Hook::add_action('handle_500_error', function ($msg, $url = null) {
             ErrorDisplay::http_error(500, $msg);
         });
 	}
